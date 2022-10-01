efec269f3fbe748bc8e0b2e98b498b6f2a4c9734
Sebastian <sa224ny@student.lnu.se>
2022-10-01 16:17:09 +0200

    Add: Error handling with the help of http-errors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,30 @@
-import 'dotenv/config'
-import express, { Express, Request, Response } from 'express'
+import express, { Express } from 'express'
+import { indexRouter } from './routes/router.js'
 
 const run = () => {
   const server: Express = express()
 
   const PORT: number = Number(process.env.PORT) || 5050
 
-  server.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'Yolo' })
+  server.use(indexRouter)
+
+  server.use((
+    err: { status: number; message: string; stack: string },
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    interface ErrorResponse {
+      status: number
+      message: string
+      stack?: string
+    }
+    const errorResponse: ErrorResponse = {
+      status: err.status || 500,
+      message: err.message || 'Internal Server Error',
+      stack: process.env.NODE_ENV !== 'production' ? err.stack : undefined,
+    }
+    res.status(errorResponse.status).json(errorResponse)
   })
 
   server.listen(PORT, () => {
