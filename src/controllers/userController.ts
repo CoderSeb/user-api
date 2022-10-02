fb5ef9b05dde6078ed5a82f696ae7c9e47c38899
Sebastian <sa224ny@student.lnu.se>
2022-10-02 15:34:03 +0200

    Add: user controller functionality for user signup

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,38 @@
-import express from 'express';
-
+import express from 'express'
+import { UserModel } from '../models/userModel.js'
 class UserController {
-  async index(req: express.Request, res: express.Response, next: express.NextFunction) {
+  async index(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
     try {
-      res.json({ message: 'User route reached..' });
+      res.json({ message: 'User route reached..' })
     } catch (err) {
-      next(err);
+      next(err)
+    }
+  }
+
+  async signUp(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const { firstName, lastName, email, pass } = req.body
+
+      const newUser = new UserModel({
+        fName: firstName,
+        lName: lastName,
+        email,
+        password: pass
+      })
+
+      await newUser.save()
+
+      res.status(201).json({ message: 'User created successfully!' })
+    } catch (err) {
+      next(err)
     }
   }
 }
