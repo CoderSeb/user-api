9924c9f0e134418c6fb4d9c8ee55fab188a25ab2
Sebastian <sa224ny@student.lnu.se>
2022-10-02 15:34:46 +0200

    Add: signup route to userRouter

--- src/routes/api/userRouter.ts
+++ src/routes/api/userRouter.ts
@@ -6,7 +6,7 @@ const router = express.Router()
 const controller = new UserController()
 
 router.get('/', controller.index)
-
+router.post('/signup', controller.signUp)
 
 export { router as userRouter }
 
