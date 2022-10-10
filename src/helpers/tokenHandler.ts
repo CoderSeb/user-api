fdba9656e190ffadfd370f459eecce2d9e337810
Sebastian <sa224ny@student.lnu.se>
2022-10-10 17:04:22 +0200

    Add: verify jwt method

--- src/helpers/tokenHandler.ts
+++ src/helpers/tokenHandler.ts
@@ -1,8 +1,16 @@
+import createError from 'http-errors'
 import jwt from 'jsonwebtoken'
-
-export interface TokenUserPayload {
+export interface ITokenUserPayload {
   id: string,
   email: string
+}
+
+export interface ITokenInfo {
+  id: string,
+  email: string,
+  iat: number,
+  exp: number,
+  iss: string
 }
 
 export class TokenHandler {
@@ -12,13 +20,32 @@ export class TokenHandler {
     this.privKey = Buffer.from(`${process.env.JWT_PRIVATE}`, 'base64').toString('ascii')
   }
 
-  public async getAccessToken(data: TokenUserPayload) {
+  public async getAccessToken(data: ITokenUserPayload) {
     const signOptions: jwt.SignOptions = {
       issuer: 'Newsflash User API',
-      algorithm: 'RS256',
+      algorithm: 'RS512',
       expiresIn: '1h'
     }
     const token = jwt.sign(data, this.privKey, signOptions)
     return token
+  }
+
+  public async verifyAccessToken(token: string): Promise<ITokenInfo> {
+    try {
+      const verifyOptions: jwt.VerifyOptions = {
+        issuer: 'Newsflash User API',
+        algorithms: ['RS512']
+      }
+      const data = jwt.verify(token, this.privKey, verifyOptions)
+      return data as ITokenInfo
+    } catch (err) {
+      if (err instanceof jwt.TokenExpiredError) {
+        throw createError(401, 'Token expired!')
+      }
+      if (err instanceof jwt.JsonWebTokenError) {
+        throw createError(401, 'Invalid token!')
+      }
+      throw err
+    }
   }
 }
