c956161d4e82524ebab503447a8bd26093b60098
Sebastian <sa224ny@student.lnu.se>
2022-10-10 13:23:06 +0200

    Refactor: changed password hashing to match mongodb documentation

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -33,13 +33,24 @@ const schema: mongoose.Schema<IUser> = new mongoose.Schema({
   }
 })
 
-schema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
-  const hash = await bcrypt.hash(this.password, 10)
-  this.password = hash
-  next()
+schema.pre('save', function (next) {
+  let user: IUser = this
+  if (!user.isModified('password')) return next()
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err)
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err)
+
+      user.password = hash
+      next()
+    })
+  })
 })
+
 
 const UserModel = mongoose.model<IUser>('User', schema)
 
 export { UserModel }
+
